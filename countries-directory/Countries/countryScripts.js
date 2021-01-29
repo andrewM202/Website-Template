ee314737455256836e7ade5bf1fbf3f90420e9c2
andrewM202 <70407217+andrewM202@users.noreply.github.com>
2021-01-29 14:20:24 -0500

    Update countryScripts.js

--- countries-directory/Countries/countryScripts.js
+++ countries-directory/Countries/countryScripts.js
@@ -4,7 +4,6 @@ let displays = document.querySelectorAll('.display > div')
 let economyDisplay = document.querySelector('.economy-display');
 let cultureDisplay = document.querySelector('.culture-display');
 let demographicsDisplay = document.querySelector('.demographics-display');
-let politicsDisplay = document.querySelector('.politics-display');
 let graphsDisplay = document.querySelector('.graphs-display');
 let options = document.querySelectorAll('header > div');
 
@@ -19,9 +18,6 @@ const largeDisplay = function (event) {
   } else if(event.target.querySelector('p').textContent === "Immigration") {
     displays.forEach((el) => el.style.display = 'none');
     demographicsDisplay.style.display = 'block';
-  } else if(event.target.querySelector('p').textContent === "Politics") {
-    displays.forEach((el) => el.style.display = 'none');
-    politicsDisplay.style.display = 'block';
   } else if(event.target.querySelector('p').textContent === "Images / Graphs") {
     displays.forEach((el) => el.style.display = 'none');
     graphsDisplay.style.display = 'block';
